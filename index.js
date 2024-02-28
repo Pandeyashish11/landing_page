ab6ed44908c0bdb84991941a796d72813b0ac8b8
admin <pandeyashishsep11@gmail.com>
2024-02-28 09:59:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".icon").click(function(){
+      $(".search").slideToggle("slow");
+    });
+
+    $("#bar").click(function(){
+      $(".listsMnav").slideToggle("slow");
+    });
+
+  });
+
+  
